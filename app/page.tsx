7678cf512fe0fd20abf6878d79de8e6bac17b0cc
Wiley Swikit <wileyswikit@gmail.com>
2025-01-20 20:58:57 -0400

    Updated dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,8 @@ export default function Home() {
 				Wiley T. Swikit
 			</h1>
 			<p>
-			Welcome to my game development portfolio! I&apos;m Wiley a self-taught game developer specializing in Unity and C#. 
-			For the past six years, I&apos;ve been making simple games that a wide audience can enjoy. 
-			As I prepare to start my university journey this September, I&apos;m excited to expand my skills and collaborate on new projects. 
-			Explore my showcased games and join me on this thrilling journey into the world of game development. 
-			Let&apos;s create experiences that inspire and entertain. <br/><br/> <b>Thank you for visiting!</b>
+			Welcome to my game development portfolio! I&apos;m a self-taught game developer specializing in Unity and C# and 
+			I&apos;ve been making games since 2019.
 			</p>
 		</>
 	)
