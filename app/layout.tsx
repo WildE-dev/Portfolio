731c5f95d7b201fe2cc3ae9c9558ca92f4aee8ff
Wiley Swikit <wileyswikit@gmail.com>
2025-01-20 21:28:39 -0400

    Added social links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,13 +23,31 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className + " bg-slate-900 text-white"}>
-				<Navbar/>
+				<Navbar />
 				<div className='px-[15%] py-6'>
 					{children}
 				</div>
 				<footer className='flex justify-between py-4 px-[15%] bg-slate-700'>
+					<div className='flex justify-between space-x-2'>
 						<p>Wiley Swikit</p>
-						<p>Copyright © {dt.getFullYear()}</p>
+						<p>&mdash;</p>
+						<a
+							className='py-1'
+							href="https://www.linkedin.com/in/wiley-swikit/"
+							target="_blank"
+						>
+							<BsLinkedin />
+						</a>
+
+						<a
+							className='py-1'
+							href="https://github.com/WildE-dev"
+							target="_blank"
+						>
+							<FaGithubSquare />
+						</a>
+					</div>
+					<p>Copyright © {dt.getFullYear()}</p>
 				</footer>
 			</body>
 		</html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,35 @@
 import Image from 'next/image'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithubSquare } from 'react-icons/fa'
 
 export default function Home() {
 	return (
 		<>
-			<h1 className='text-8xl text-center my-8 py-16 bg-local bg-cover rounded-lg text-slate-900 font-bold main-title'>
+			<h1 className='text-8xl text-center py-16 bg-local bg-cover rounded-lg text-slate-900 font-bold main-title'>
 				Wiley T. Swikit
 			</h1>
-			<p>
-			Welcome to my game development portfolio! I&apos;m a self-taught game developer specializing in Unity and C# and 
-			I&apos;ve been making games since 2019.
+			<p className='w-2/3 m-auto text-center p-5'>
+				Welcome to my game development portfolio! I&apos;m a self-taught game developer specializing in Unity and C# and
+				I&apos;ve been making games since 2019. Currently seeking a position as a software developer.
 			</p>
+			<div className="flex flex-col sm:flex-row items-center justify-center gap-3 px-4 pt-2 text-lg font-medium">
+				<a
+					className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+					href="https://www.linkedin.com/in/wiley-swikit/"
+					target="_blank"
+				>
+					<BsLinkedin />
+					<b>LinkedIn</b>
+				</a>
+				<a
+					className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+					href="https://github.com/WildE-dev"
+					target="_blank"
+				>
+					<FaGithubSquare />
+					<b>GitHub</b>
+				</a>
+			</div>
 		</>
 	)
 }
